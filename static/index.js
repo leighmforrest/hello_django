4531fdb1b09f1f111384ed0b964fc7ce0ff7c8e6
Leigh Michael Forrest <leighmforrest@gmail.com>
2025-02-12 16:54:08 -0500

    message partial added; logout functional

--- static/index.js
+++ static/index.js
@@ -1,12 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
     const hamburger = document.getElementById("hamburger");
     const menu = document.getElementById("nav-links");
+    const alertCloseButtons = document.querySelectorAll(".close-alert");
 
-    function toggleMenu() {
+    const toggleMenu = () => {
         menu.classList.toggle("active");
         menu.classList.toggle("hidden");
         hamburger.classList.toggle("open");
-    }
+    };
+
+    const alertCloseButtonHandler = function() {
+        this.closest(".alert-box").remove();
+    };
 
     hamburger.addEventListener("click", toggleMenu);
+    alertCloseButtons.forEach(button => button.addEventListener("click", alertCloseButtonHandler));
 });
+
